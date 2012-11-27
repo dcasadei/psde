44ccc815ecdfb7e49b4fc92bb68c91687701eca9
casadei <casadei@4525493e-7705-40b1-a816-d608a930855b>
2012-11-27 08:21:58 +0000

    using approx expression for normal quantile from http://home.online.no/~pjacklam/notes/invnorm/

--- pValuePoissonError.C
+++ pValuePoissonError.C
@@ -229,6 +229,148 @@ double pValuePoissonError(unsigned nObs, // observed counts
 
 
 
+
+
+/*
+  Normal quantile computed following Peter John Acklam's
+  pseudo-code algorithm for rational approximation
+  (pjacklam@online.no) 
+  http://home.online.no/~pjacklam/notes/invnorm/
+
+  The algorithm below assumes p is the input and x is the output.
+
+   Coefficients in rational approximations.
+   a(1) <- -3.969683028665376e+01
+   a(2) <-  2.209460984245205e+02
+   a(3) <- -2.759285104469687e+02
+   a(4) <-  1.383577518672690e+02
+   a(5) <- -3.066479806614716e+01
+   a(6) <-  2.506628277459239e+00
+
+   b(1) <- -5.447609879822406e+01
+   b(2) <-  1.615858368580409e+02
+   b(3) <- -1.556989798598866e+02
+   b(4) <-  6.680131188771972e+01
+   b(5) <- -1.328068155288572e+01
+
+   c(1) <- -7.784894002430293e-03
+   c(2) <- -3.223964580411365e-01
+   c(3) <- -2.400758277161838e+00
+   c(4) <- -2.549732539343734e+00
+   c(5) <-  4.374664141464968e+00
+   c(6) <-  2.938163982698783e+00
+
+   d(1) <-  7.784695709041462e-03
+   d(2) <-  3.224671290700398e-01
+   d(3) <-  2.445134137142996e+00
+   d(4) <-  3.754408661907416e+00
+
+   Define break-points.
+
+   p_low  <- 0.02425
+   p_high <- 1 - p_low
+
+   Rational approximation for lower region.
+
+   if 0 < p < p_low
+      q <- sqrt(-2*log(p))
+      x <- (((((c(1)*q+c(2))*q+c(3))*q+c(4))*q+c(5))*q+c(6)) /
+            ((((d(1)*q+d(2))*q+d(3))*q+d(4))*q+1)
+   endif
+
+   Rational approximation for central region.
+
+   if p_low <= p <= p_high
+      q <- p - 0.5
+      r <- q*q
+      x <- (((((a(1)*r+a(2))*r+a(3))*r+a(4))*r+a(5))*r+a(6))*q /
+           (((((b(1)*r+b(2))*r+b(3))*r+b(4))*r+b(5))*r+1)
+   endif
+
+   Rational approximation for upper region.
+
+   if p_high < p < 1
+      q <- sqrt(-2*log(1-p))
+      x <- -(((((c(1)*q+c(2))*q+c(3))*q+c(4))*q+c(5))*q+c(6)) /
+             ((((d(1)*q+d(2))*q+d(3))*q+d(4))*q+1)
+   endif
+
+  The absolute value of the relative error (x_approx - x)/x is
+  <= 1.15e-9 for all input values p which can be represented in
+  IEEE double precision arithmetic.
+ */
+double pja_normal_quantile(long double p) {
+
+  long double a[6] = {
+    -3.969683028665376e+01, // a(1) -> a[0]
+     2.209460984245205e+02, // a(2)
+    -2.759285104469687e+02, // a(3)
+     1.383577518672690e+02, // a(4)
+    -3.066479806614716e+01, // a(5)
+     2.506628277459239e+00, // a(6) -> a[5]
+  };
+
+  long double b[5] = {
+    -5.447609879822406e+01, // b(1) -> b[0]
+     1.615858368580409e+02, // b(2)
+    -1.556989798598866e+02, // b(3)
+     6.680131188771972e+01, // b(4)
+    -1.328068155288572e+01, // b(5) -> b[4]
+  };
+
+  long double c[6] = {
+    -7.784894002430293e-03, // c(1) -> c[0]
+    -3.223964580411365e-01, // c(2)
+    -2.400758277161838e+00, // c(3)
+    -2.549732539343734e+00, // c(4)
+     4.374664141464968e+00, // c(5)
+     2.938163982698783e+00, // c(6) -> c[5]
+  };
+
+  long double d[4] = {
+     7.784695709041462e-03, // d(1) -> d[0]
+     3.224671290700398e-01, // d(2)
+     2.445134137142996e+00, // d(3)
+     3.754408661907416e+00, // d(4) -> d[3]
+  };
+
+  // Define break-points.
+  long double p_low  = 0.02425;
+  long double p_high = 1 - p_low;
+
+  // output value
+  double x=0;
+
+  // Rational approximation for lower region.
+  if (0 < p && p < p_low) {
+    long double q = sqrt(-2*log(p));
+    x = (((((c[0]*q+c[1])*q+c[2])*q+c[3])*q+c[4])*q+c[5]) /
+	((((d[0]*q+d[1])*q+d[2])*q+d[3])*q+1);
+  }
+  // Rational approximation for central region.
+  else if (p_low <= p && p <= p_high) {
+    long double q = p - 0.5;
+    long double r = q*q;
+    x = (((((a[0]*r+a[1])*r+a[2])*r+a[3])*r+a[4])*r+a[5])*q /
+      (((((b[0]*r+b[1])*r+b[2])*r+b[3])*r+b[4])*r+1);
+  }
+  // Rational approximation for upper region.
+  else if (p_high < p && p < 1) {
+    long double q = sqrt(-2*log(1-p));
+    x = -(((((c[0]*q+c[1])*q+c[2])*q+c[3])*q+c[4])*q+c[5]) /
+      ((((d[0]*q+d[1])*q+d[2])*q+d[3])*q+1);
+  }
+
+  return x;
+}
+
+
+
+
+
+
+
+
 /*
   Convert a p-value into a right-tail normal significance, i.e. into
   the number of Gaussian standard deviations which correspond to it.
@@ -252,10 +394,16 @@ double pValueToSignificance(double p,     // p-value
     cerr << "ERROR: p-value must belong to [0,1] but input value is " << p << endl;
     return 0;
   }
-
+  /*
   if (excess) 
     return ROOT::Math::normal_quantile(1-p,1);
   else
     return ROOT::Math::normal_quantile(p,1);
+  */
+
+  if (excess) 
+    return pja_normal_quantile(1-p);
+  else
+    return pja_normal_quantile(p);
 }
 
